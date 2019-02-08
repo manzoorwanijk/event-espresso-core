5beddbdc9bb7af7f37d06c147a2d11b7808b9191
Josh Feck <josh@nxnweb.com>
2019-02-08 15:13:47 -0500

    allow line items and price money fields to be more precise (we will want to sometiems round line items, that logic to come)

--- core/db_models/EEM_Line_Item.model.php
+++ core/db_models/EEM_Line_Item.model.php
@@ -167,7 +167,8 @@ class EEM_Line_Item extends EEM_Base
                     'LIN_unit_price',
                     esc_html__('Unit Price', 'event_espresso'),
                     false,
-                    0
+                    0,
+                    false
                 ),
                 'LIN_percent'    => new EE_Float_Field(
                     'LIN_percent',
@@ -191,7 +192,8 @@ class EEM_Line_Item extends EEM_Base
                     'LIN_total',
                     esc_html__('Total (unit price x quantity)', 'event_espresso'),
                     false,
-                    0
+                    0,
+                    false
                 ),
                 'LIN_quantity'   => new EE_Integer_Field(
                     'LIN_quantity',
